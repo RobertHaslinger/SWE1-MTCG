a72a2b365e1689d981a1c1c4dad1fd0e8c7e1b1b
Robert Haslinger <if20b189@technikum-wien.at>
2020-10-11 13:45:35 +0200

    add StoreTest, Store

---
+++ SWE1-MTCG.Test/StoreTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace SWE1_MTCG.Test
+{
+    [TestFixture]
+    public class StoreTest
+    {
+
+        private Store _store;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //common arrange
+            //TODO add WebApi in constructor after WebService project is finished
+            _store= new Store();
+        }
+
+        [TestCase("unknownUser")]
+        public void Test_StoreGetOpenTradingDealsShouldReturnEmptyIEnumberableWhenUnknownUser(string username)
+        {
+            //act
+            IEnumerable tradingDealsForUser = _store.GetOpenTradingDealsByUser(username);
+
+            //assert
+            Assert.IsEmpty(tradingDealsForUser);
+        }
+    }
+}
---
+++ SWE1-MTCG/Store.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWE1_MTCG
+{
+    public class Store
+    {
+
+        #region fields
+
+        #endregion
+
+        #region properties
+
+        #endregion
+
+        #region constructor
+
+        #endregion
+
+        #region private methods
+
+        #endregion
+
+        #region public methods
+        public IEnumerable GetOpenTradingDealsByUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+    }
+}
